2744786665774e86a51c5efe372f9884160527d0
Sourav0879 <145244763+Sourav0879@users.noreply.github.com>
2025-02-22 15:44:28 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from client import Bot
 import pyrogram.utils
 
-pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
+pyrogram.utils.MIN_CHANNEL_ID = -1002386986283
 print("Prime Post Finder Bot Started 💥")
 Bot().run()
