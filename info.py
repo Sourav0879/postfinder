6783328ba586020f86e29469da1c4e24663cb970
Sourav0879 <145244763+Sourav0879@users.noreply.github.com>
2025-02-22 13:14:45 +0600

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
-API_ID       = 
-API_HASH     = ""
+API_ID       = 21100083
+API_HASH     = "8980ecdc34880454c2e31eec10033f5b"
 BOT_TOKEN    = ""
 SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -100
-ADMIN        = 
+DATABASE_URI = "mongodb+srv://polay60829:polay60829@cluster0.2qxpo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+LOG_CHANNEL  = -1002386986283
+ADMIN        = 7594442610
