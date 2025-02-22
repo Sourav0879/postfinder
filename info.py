5a40555cfa0622a5ff851f03aa0fe750809d3520
Sourav0879 <145244763+Sourav0879@users.noreply.github.com>
2025-02-22 18:35:18 +0600

    Update info.py

--- info.py
+++ info.py
@@ -2,6 +2,6 @@ API_ID       = 21100083
 API_HASH     = "8980ecdc34880454c2e31eec10033f5b"
 BOT_TOKEN    = "7960853627:AAFH_9896KR40wYKfhMxwDGybFG-xww6OsM"
 SESSION      = "BQFB9jMARMpSHGH6U3u-bWlx2K3CvNHs4rc_j0O2Mz5WrXAOH_ilBAqY8rlPTDMH-k9dHx4ipKcKRmhSVBpM2z5ZVp7cim2KiYkeC02FFg_mVgvLpSYSPmRlNNujS3lOZ3KsHHoA6PVpyfeKXIpY9PXNrXQzBEiLILMZeqTF2eSJmTCK6dwuvhVF06TyH88lRi7jKvnWwSXS70Fx42vEhaaDy2u7uHuD59G6IJsz2ZPfq9K8vURVbuhhDi84PJIUXkY5M_UGEVYTNJYlE36M1yXNN_GCICF-86459VKUkFMiTJIF7W5WGXIb99EBFRxscZSHx2smjDp2mmqgvtRU3N_iFWTuyAAAAAHEqf9yAA"
-DATABASE_URI = "mongodb+srv://polay60829:polay60829@cluster0.2qxpo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE_URI = "mongodb+srv://infusedraindrops126:oQ6zaL4omfcdOGAt@cluster0.1kt0y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 LOG_CHANNEL  = -1002386986283
 ADMIN        = 7594442610
